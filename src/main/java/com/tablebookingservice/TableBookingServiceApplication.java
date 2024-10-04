3833845e3d82b2f3924b7656c64903b802301f08
hjkim <hjhjgudwls1022@gmail.com>
2024-10-04 20:58:36 +0900

    Initial commit

---
+++ src/main/java/com/tablebookingservice/TableBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tablebookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableBookingServiceApplication.class, args);
+    }
+
+}
