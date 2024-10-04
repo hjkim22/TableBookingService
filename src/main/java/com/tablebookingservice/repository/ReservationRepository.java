0d91ea853958713aa4f7c51ec14427a28822b917
hjkim <hjhjgudwls1022@gmail.com>
2024-10-05 01:58:03 +0900

    reset

--- src/main/java/com/tablebookingservice/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tablebookingservice.repository;
-
-import com.tablebookingservice.domain.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
--- src/main/java/com/tablebookingservice/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tablebookingservice.repository;
-
-import com.tablebookingservice.domain.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
