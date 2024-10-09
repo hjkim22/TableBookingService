f0d970000b7f040d6f20bab33852e19fd4575438
hjkim <hjhjgudwls1022@gmail.com>
2024-10-09 20:26:10 +0900

    review controller update

--- src/main/java/com/tablebookingservice/review/controller/ReviewController.java
+++ src/main/java/com/tablebookingservice/review/controller/ReviewController.java
@@ -15,20 +15,32 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
     private final ReviewService reviewService;
 
-    // 리뷰 생성
+    /**
+     * 리뷰 생성
+     * @param userId        리뷰 작성 사용자 ID
+     * @param storeId       리뷰 대상 가게 ID
+     * @param reservationId 리뷰 관련 예약 ID
+     * @param request       리뷰 세부 정보 포함 요청 본문
+     * @return              생성 리뷰 포함 응답
+     */
     @PostMapping("/create")
     @PreAuthorize("hasRole('ROLE_USER')")
     public CreateReview.Response createReview(
-            @RequestParam(name = "userid") Long userId,
-            @RequestParam(name = "storeid") Long storeId,
-            @RequestParam(name = "reservationid") Long reservationId,
+            @RequestParam(name = "userId") Long userId,
+            @RequestParam(name = "storeId") Long storeId,
+            @RequestParam(name = "reservationId") Long reservationId,
             @RequestBody CreateReview.Request request) {
 
         return CreateReview.Response.fromReviewDto(
                 this.reviewService.createReview(userId, storeId, reservationId, request));
     }
 
-    // 리뷰 업데이트
+    /**
+     * 리뷰 수정
+     * @param reviewId 수정할 리뷰 ID
+     * @param request  수정할 리뷰 세부 정보 포함 요청 본문
+     * @return         수정된 리뷰 포함 응답
+     */
     @PutMapping("/update/{reviewId}")
     @PreAuthorize("hasRole('ROLE_USER')")
     public UpdateReview.Response updateReview(
@@ -38,7 +50,11 @@ public class ReviewController {
                 this.reviewService.updateReview(reviewId, request));
     }
 
-    // 리뷰 삭제
+    /**
+     * 리뷰 삭제
+     * @param id 삭제 리뷰 ID
+     * @return   삭제 결과 응답
+     */
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_MANAGER')")
     public ResponseEntity<?> deleteReview(@PathVariable Long id) {
